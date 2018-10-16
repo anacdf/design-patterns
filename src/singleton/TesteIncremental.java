f1cf8ffd090a978fd6494237c582e091ee8e0ed4
Ana Carolina <anadedavid@gmail.com>
2018-10-16 18:31:24 -0300

    singleton

--- src/singleton/TesteIncremental.java
+++ src/singleton/TesteIncremental.java
@@ -3,21 +3,29 @@ package singleton;
 public class TesteIncremental {
     public static void main(String[] args) {
         for(int i=0; i<10; i++){
-            Incremental incremental = new Incremental();
+            Incremental incremental = Incremental.getInstance();
             System.out.println(incremental);
         }
     }
 
-    public static class Incremental{
+    public static final class Incremental{
         private int count = 0;
         private int numero;
 
-        public Incremental(){
+        private Incremental(){
             numero = ++count;
         }
 
-        public String toString(){
-            return "Incremental "+ numero;
+        private static class IncrementalHolder{
+            private static final Incremental INSTANCE = new Incremental();
         }
+
+        public static Incremental getInstance(){
+            return IncrementalHolder.INSTANCE;
+        }
+
+//        public String toString(){
+//            return "Incremental "+ numero;
+//        }
     }
 }
